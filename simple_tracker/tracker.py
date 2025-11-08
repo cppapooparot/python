e8f3b8955f52f277f36f7dd52d364b3cfcc76ec3
Narek <nsahakyan42@gmail.com>
2025-11-08 20:35:44 +0400

    feat: add Tracker class

---
+++ simple_tracker/tracker.py
@@ -0,0 +1,10 @@
+class Tracker:
+	def __init__(self):
+		self.count = 0
+	def increment(self):
+		self.count += 1
+	def save_to_file(self):
+		with open("data.txt", "w") as file:
+			file.write(str(self.count))
+	def __str__(self):
+		return f"Current count: {self.count}"
